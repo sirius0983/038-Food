30de49c31b5d2fd57d20d896de14708198fd6bff
Rustam Siraev <sirius0983@mail.ru>
2021-02-18 10:30:46 +0500

    73 end

---
+++ js/modules/modal.js
@@ -0,0 +1,52 @@
+function openModal(modalSelector, modalTimerID) {
+    const modal = document.querySelector(modalSelector)
+    modal.classList.add('show')
+    modal.classList.remove('hide')
+    document.body.style.overflow = 'hidden'
+    if (modalTimerID) {
+        clearInterval(modalTimerID)
+    }
+}
+
+function closeModal(modalSelector) {
+    const modal = document.querySelector(modalSelector)
+    modal.classList.add('hide')
+    modal.classList.remove('show')
+    document.body.style.overflow = ''
+}
+
+function modal(triggerSelector, modalSelector, modalTimerID) {
+
+    let modalTrigger = document.querySelectorAll(triggerSelector),
+        modal = document.querySelector(modalSelector)
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => openModal(modalSelector, modalTimerID))
+    })
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute("data-close") === '') {
+            closeModal(modalSelector)
+        }
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal(modalSelector)
+        }
+    })
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal(modalSelector, modalTimerID)
+            window.removeEventListener('scroll', showModalByScroll)
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll)
+
+}
+
+export default modal
+export {closeModal}
+export {openModal}
